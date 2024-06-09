4bb09ace97b37d55046a60cdfbaa151c50e6271c
Shubhamkrishna0 <krishnashubham09@gmail.com>
2024-06-09 10:05:33 +0530

    little nikita

---
+++ Little Nikita.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        if (n >= m && n % 2 == m % 2) {
+            cout << "YES\n";
+        } else {
+            cout << "NO\n";
+        }
+    }
+    return 0;
+}
